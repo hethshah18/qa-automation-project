36413f7086ef656220c1b43fa5ebdc0f41c87771
het <hethshah2000@gmail.com>
2025-12-23 13:13:28 +0530

    Initialize project structure with Maven configuration and Selenium test setup

---
+++ Testing/src/sel_test/sele.java
@@ -0,0 +1,25 @@
+package sel_test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.lang.ClassNotFoundException;
+//import com.google.common.net.MediaType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class sele {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://dashboard.beawms.com/");
+		driver.findElement(By.id("email")).sendKeys("admin@aits.com");
+		driver.findElement(By.id("password")).sendKeys("admiN@123$");
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/form/button")).click();
+		Thread.sleep(10000);
+		driver.quit();
+	}
+
+}
