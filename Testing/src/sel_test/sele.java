8317f87c1166d2cd46d667321122dec980b8dd98
het <hethshah2000@gmail.com>
2025-12-24 18:31:20 +0530

    Update Selenium test to log in to Facebook and remove commented-out code for dashboard login

--- Testing/src/sel_test/sele.java
+++ Testing/src/sel_test/sele.java
@@ -14,12 +14,20 @@ public class sele {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://dashboard.beawms.com/");
-		driver.findElement(By.id("email")).sendKeys("admin@aits.com");
-		driver.findElement(By.id("password")).sendKeys("admiN@123$");
-		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/form/button")).click();
-//		 Thread.sleep(10000);
+		// driver.get("https://dashboard.beawms.com/");
+		// driver.findElement(By.id("email")).sendKeys("admin@aits.com");
+		// driver.findElement(By.id("password")).sendKeys("admiN@123$");
+		// driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/form/button")).click();
+		//  Thread.sleep(10000);
+		// driver.quit();
+
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("hethshah2000@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Het@1234");
+		driver.findElement(By.xpath("(//*[contains(text(),'Log in')])[1]")).click();
+		Thread.sleep(5000);
 		driver.quit();
+
 	}
 
 }
