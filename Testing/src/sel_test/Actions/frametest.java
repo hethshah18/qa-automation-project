3fd3c2ef1e4cf566638a8f0b6445166c92f78106
het <hethshah2000@gmail.com>
2026-01-30 16:02:57 +0530

    Add frame handling test; implement draggable element interaction in new frametest class

---
+++ Testing/src/sel_test/Actions/frametest.java
@@ -0,0 +1,17 @@
+package sel_test.Actions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class frametest {
+    public static void main(String[] args) {
+        
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://jqueryui.com/droppable/");
+        driver.switchTo().frame(0);
+        driver.findElement(By.id("draggable")).click();//throws NoSuchElementException
+        
+    }
+}
