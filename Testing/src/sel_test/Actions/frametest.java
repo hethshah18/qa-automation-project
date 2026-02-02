e68d7002b276ecdfa8604779b0d1467150d50015
het <hethshah2000@gmail.com>
2026-02-02 18:43:06 +0530

    Update frametest class; implement drag-and-drop functionality and enhance frame handling in Selenium tests

--- Testing/src/sel_test/Actions/frametest.java
+++ Testing/src/sel_test/Actions/frametest.java
@@ -1,8 +1,12 @@
 package sel_test.Actions;
 
+import javax.swing.Action;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class frametest {
     public static void main(String[] args) {
@@ -10,8 +14,11 @@ public class frametest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://jqueryui.com/droppable/");
-        driver.switchTo().frame(0);
-        driver.findElement(By.id("draggable")).click();//throws NoSuchElementException
-        
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame")));
+ 
+        Actions a = new Actions(driver);
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droppable"));
+        a.dragAndDrop(source, target).build().perform();
     }
 }
