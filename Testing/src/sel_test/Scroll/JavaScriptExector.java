e0eb8dc430c3d8b19989b19d29e2266d7ce4a1a7
het <hethshah2000@gmail.com>
2026-03-02 18:45:17 +0530

    Refactor JavaScriptExector class; add functionality to calculate and assert sum of table values in Selenium tests

--- Testing/src/sel_test/Scroll/JavaScriptExector.java
+++ Testing/src/sel_test/Scroll/JavaScriptExector.java
@@ -1,9 +1,14 @@
 package sel_test.Scroll;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.idealized.Javascript;
+import org.testng.Assert;
 
 public class JavaScriptExector {
     public static void main(String[] args) throws InterruptedException {
@@ -17,5 +22,17 @@ public class JavaScriptExector {
         js.executeScript("window.scrollBy(0,500)");
         Thread.sleep(2000);
         js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+
+        List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+
+        int sum = 0;
+        for(int i=0; i<values.size(); i++){
+            
+            sum = sum + Integer.parseInt(values.get(i).getText());
+        }
+        System.out.println(sum);
+
+        int total = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+        Assert.assertEquals(sum, total);
     }
 }
